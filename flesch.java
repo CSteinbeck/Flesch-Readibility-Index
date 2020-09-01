435fdbc344a34005b7d0dc72c248879b2c9157fe
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2020-09-01 11:23:33 -0400

    Modifications to Flesch and Perl tokenizer file

--- flesch.java
+++ flesch.java
@@ -1,2 +1,9 @@
 //Colton Steinbeck//
 //Test comment for the file transfer on Anvil//
+Class flesch
+{
+    public static void main(String[] args)
+    {
+     System.out.println("Hello world!");
+    }
+}
