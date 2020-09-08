ea4f9c597db8a188ff9ee04fdf4f0ec8b650dbd6
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2020-09-07 22:20:10 -0400

    Updates to Java File

--- flesch.java
+++ flesch.java
@@ -1,9 +1,37 @@
 //Colton Steinbeck//
 //Test comment for the file transfer on Anvil//
-Class flesch
-{
-    public static void main(String[] args)
+import java.lang.*;
+import java.util. *;
+import java.io.*;
+
+public class flesch
+{    
+
+    public static void main(Strings[] args) 
     {
-     System.out.println("Hello world!");
+        //Takes the users command line input
+    System.out.println("Please enter the file location of the translations you would like to use: ");
+    Scanner tScan = new Scanner(System.in);
+    String translation = tScan.nextline();
+
+    //Takes the file path puts these words into the array//
+    File translation - new File();
+    Scanner scan = new Scanner(File(translation));
+     
+    ArrayList<String> vals= new ArrayList<String>();
+        while(scan.hasNext())
+        {
+            String words= scan.next();
+            vals.add(words);
+        }
+
+    
+        
+
+
+
+
     }
+
+
 }
